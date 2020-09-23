a32d3b09e6da1558f781af87011f843fad249fbe
S-Z-all <59140418+Z-all-S@users.noreply.github.com>
2020-09-23 22:45:29 +0800

    Update TestMain.java

--- src/TestMain.java
+++ src/TestMain.java
@@ -2,9 +2,12 @@ import org.junit.Test;
 
 public class TestMain {
     @Test
-    public void test(){
-        String[] args = {"D://test//orig.txt","D://test//orig2.txt","D://test//result.txt"};
+    public void test() {
+        String[] args = {"D://test//orig.txt", "D://test//orig2.txt", "D://test//result.txt"};
         DuplicateCheck.main(args);
+        try {
+            Thread.sleep(200 * 1000);
+        }catch (InterruptedException ignore){}
     }
 
 
